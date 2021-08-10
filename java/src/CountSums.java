43e6d2f86c408f9e396d2028003e2a195d2b0908
Nathan Li <nathan.li.1995@gmail.com>
2021-08-10 17:58:42 -0400

    solved bug of many duplicates in question array

--- java/src/CountSums.java
+++ java/src/CountSums.java
@@ -7,13 +7,13 @@ public class CountSums {
         // setup hashset for quick access
         HashMap<Integer, Integer> data = new HashMap<>();
         for (int i = 0; i < array.length; i++) {
-            data.put(array[i], 0);
-
+            if (!data.containsKey(array[i]))
+                data.put(array[i], 0);
         }
 
         for (int j = 0; j < array.length; j++) {
             int b = sums - array[j];
-            if (data.get(array[j]) == 1)
+            if (data.get(array[j]) == 1 || data.get(b) == 1)
                 continue;
             else if (data.containsKey(b)) {
                 count++;
@@ -27,7 +27,7 @@ public class CountSums {
     public static void main(String args[]) {
         CountSums cs = new CountSums();
 //        int[] the_array = new int[]{1,5,7,-1};
-        int[] the_array = new int[]{1,5,7,-1,5};
+        int[] the_array = new int[]{1,1,5,7,-1,5,5};
         int sums = 6;
         System.out.println(cs.count_sums(the_array, sums));
     }
